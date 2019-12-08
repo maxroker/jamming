aba5f713c64f61939d3c5b89b6b5c253e8e3bb59
DankoMaksym <dankomaksym@gmail.com>
2019-12-09 00:51:14 +0200

    Add & delete functionality added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,24 +12,58 @@ class App extends React.Component {
       id: 1,
       name: 'whole lotta love',
       artist: 'led zeppelin',
-      album: 'led zeppelin I',
-      isRemoval: true }, {
+      album: 'led zeppelin I'}, {
       id: 2,
       name: 'imigrant song',
       artist: 'led zeppelin',
-      album: 'led zeppelin I',
-      isRemoval: false }, {
+      album: 'led zeppelin I'}, {
       id: 3,
       name: 'rock \'n\' roll',
       artist: 'led zeppelin',
-      album: 'led zeppelin I',
-      isRemoval: false }, {
+      album: 'led zeppelin I'}, {
       id: 4,
       name: 'babe, I\'m gonna leave you',
       artist: 'led zeppelin',
-      album: 'led zeppelin I',
-      isRemoval: false }]
+      album: 'led zeppelin I'}];
+    this.state.playlistTracks = [{
+      id: 5,
+      name: 'moby dick',
+      artist: 'led zeppelin',
+      album: 'led zeppelin I'}, {
+      id: 6,
+      name: 'lemon song',
+      artist: 'led zeppelin',
+      album: 'led zeppelin I'}, {
+      id: 7,
+      name: 'double d',
+      artist: 'led zeppelin',
+      album: 'led zeppelin I'}, {
+      id: 8,
+      name: 'babe, I\'m gonna leave you',
+      artist: 'led zeppelin',
+      album: 'led zeppelin I'}];
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
     };
+
+  addTrack(track) {
+    if (this.state.playlistTracks.every(savedTrack => savedTrack.id !== track.id)) {   
+      this.state.playlistTracks.push(track);
+      //console.log(this.state.playlistTracks);
+      this.setState(this.state.playlistTracks);
+    }
+  }
+
+  removeTrack(track) {
+    console.log(track.id);
+    this.state.playlistTracks.forEach((savedTrack, index) => {
+      if (savedTrack.id === track.id) {
+        delete this.state.playlistTracks[index];
+        this.setState(this.state.playlistTracks);
+      }
+    })
+  }
 
   render() {
     return (
@@ -39,8 +73,8 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
 
-            <SearchResults searchResults={this.state.SearchResults} />
-            <Playlist playlist={this.state.SearchResults} />
+            <SearchResults searchResults={this.state.SearchResults} onAdd={this.addTrack} />
+            <Playlist playlist={this.state.playlistTracks} onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -3,17 +3,36 @@ import './Track.css';
 
 
 class Track extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack() {
+    //console.log(this.props.track);
+    this.props.onAdd(this.props.track);
+  }
+
+  removeTrack() {
+    //console.log('removing');
+    this.props.onRemove(this.props.track);
+  }
+
   renderAction() {
     let button;
-    if (this.props.track.isRemoval) {
-      button = <button className="Track-action">-</button>;
+    if (this.props.isRemoval) {
+      button = <button className="Track-action" onClick={this.removeTrack}>-</button>;
     } else {
-      button = <button className="Track-action">+</button>;
+      button = <button className="Track-action" onClick={this.addTrack} >+</button>;
     }
     return button;
   }
+
+  
   render() {
     return (
+
       <div className="Track">
         <div className="Track-information">
           <h3>{this.props.track.name}</h3>
